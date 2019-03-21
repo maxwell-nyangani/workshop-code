5256360d8cac6d330ae908cf6ffb6b4c258ff56a
m_a_x_w_e_l_l <max.nyanx@gmail.com>
2019-03-21 03:02:27 +0100

    ..

--- workShopCode/labs/step2/nonTrivialProject/nonTrivialApp/urls.py
+++ workShopCode/labs/step2/nonTrivialProject/nonTrivialApp/urls.py
@@ -1,5 +1,15 @@
 from django.conf.urls import url
 from nonTrivialApp import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+from django.views.static import serve
+
+
+@login_required
+def protected_serve(request, path, document_root=None, show_indexes=False):
+    return serve(request, path, document_root, show_indexes)
+
 
 urlpatterns = [
     url(
@@ -13,3 +23,6 @@ urlpatterns = [
         name='get_post_stores'
     )
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
