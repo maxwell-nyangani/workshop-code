269275d9e4a516a424f8743429ca1883ab76a8ae
m_a_x_w_e_l_l <max.nyanx@gmail.com>
2019-03-21 02:56:14 +0100

    ..

--- workShopCode/nonTrivialProject/nonTrivialApp/urls.py
+++ workShopCode/nonTrivialProject/nonTrivialApp/urls.py
@@ -1,5 +1,15 @@
 from django.conf.urls import url
 from nonTrivialApp import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+from django.views.static import serve
+
+
+@login_required
+def protected_serve(request, path, document_root=None, show_indexes=False):
+    return serve(request, path, document_root, show_indexes)
+
 
 urlpatterns = [
     url(
@@ -38,4 +48,14 @@ urlpatterns = [
         views.get_post_addresses,
         name='get_post_addresses'
     )
+    # ,
+    # url(
+    #     r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
+    #     protected_serve,
+    #     {'document_root': settings.MEDIA_ROOT}
+    # )
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
